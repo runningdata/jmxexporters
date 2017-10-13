0998e2c693755e171fd1360627cbc794bb86cc3a
WillCup <willcup@163.com>
2017-10-13 19:22:22 +0800

    no jmx

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,4 +14,5 @@ METRIC_FILE=/${SRV_TYPE}_sample.yaml
 echo "hostPort: ${HOST_PORT}" >> ${METRIC_FILE}
 echo "instanceName: ${INSTANCE_NAME}" >> ${METRIC_FILE}
 echo "http port is ${HTTP_PORT}, target host port is ${HOST_PORT}"
-java -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=5555 -jar /jmx_prometheus_httpserver.jar ${HTTP_PORT} ${METRIC_FILE}  
+#java -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=5555 -jar /jmx_prometheus_httpserver.jar ${HTTP_PORT} ${METRIC_FILE}  
+java -jar /jmx_prometheus_httpserver.jar ${HTTP_PORT} ${METRIC_FILE}
